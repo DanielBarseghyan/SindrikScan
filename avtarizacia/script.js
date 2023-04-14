446571771b2f7aac60f91dd87fb93e241de35fba
DanielBarseghyan <95428186+DanielBarseghyan@users.noreply.github.com>
2023-04-14 11:38:52 +0400

    Add files via upload

---
+++ avtarizacia/script.js
@@ -0,0 +1,27 @@
+if (window.localStorage.userProps) {
+    window.location.href = '../list/list.html'
+}
+const form = document.querySelector('#logForm');
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const myForm = new FormData(form);
+    console.log(...myForm);
+    //! jnjel
+
+    //!  jnjel
+    fetch("https://httpbin.org/post", {
+        method: 'POST',
+        body: myForm,
+    })
+        .then(res => {
+            if (res.ok) {
+                // key полученный ключ запроса;
+                // data для отслеживание времени авторизации;
+                // user для отображения пользователя в list.html;
+                let data = JSON.stringify({ key: 'lpo9987', date: new Date().getDate()})
+                window.localStorage.setItem('userProps', data);
+                window.location.href = '../list/list.html'
+            }
+        })
+        .catch(err => console.log(err))
+})
